be757b6718730555a34fdc7e796dd534ef3cf1a2
hongseungzz <zoxjdiwkwk@gmail.com>
2022-08-23 14:45:16 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -507,7 +507,6 @@ def Lookup():
     list_n=start.db_select(name)
     # db_select함수에 name값 입력
 
-
     if len(list_n) > 0:
         responseBody = {
             "version": "2.0",
@@ -549,5 +548,45 @@ def Lookup():
                 ]
             }  
         }
+    else:
+        responseBody = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                     {
+                        "simpleText": {
+                            
+                            "text": "없음"
+                            },
+                    },
 
+                    {
+                    "carousel": {
+                    "type": "basicCard",        
+                    "items": [
+                        {
+                        "title": list_n[0],
+                        "description": "장학금 조회"
+                        "thumbnail": {
+                            "imageUrl": "https://github.com/seungukkim/flower75982/blob/main/image/%EC%9E%A5%ED%95%99%EA%B8%881.jpg?raw=true",
+                        },
+                        "buttons": [
+                            {
+                            "action":"webLink",
+                            "label": "구경하기",
+                            "webLinkUrl": list_n[1]
+                            },
+                            {
+                            "action": "share",
+                            "label": "공유하기"
+
+                            }
+                        ]
+                        }
+                    ]      
+                    }    
+                }
+                ]
+            }  
+        }
         return responseBody
